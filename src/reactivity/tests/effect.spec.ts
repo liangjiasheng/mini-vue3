1ed1ac217af70b14c857e480de16e486e0794e23
liangjiasheng <845061953@qq.com>
2022-04-23 01:32:12 +0800

    feat: 补充 reactive & effect 测试用例

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -2,9 +2,9 @@ import reactive from '../reactive';
 import effect from '../effect';
 
 describe('effect', () => {
-  it("happy path", () => {
+  it('happy path', () => {
     const raw = {
-      count: 1
+      count: 1,
     };
 
     let double;
@@ -12,13 +12,17 @@ describe('effect', () => {
     const observed = reactive(raw);
 
     effect(() => {
+      // init -> get -> track
       double = observed.count * 2;
     });
 
+    // effect 首次执行，访问响应式数据，触发依赖收集，赋值 double
     expect(double).toBe(2);
 
+    // update -> set -> trigger -> effect
     observed.count++;
 
+    // 响应式数据更新后，重新执行 effect 函数
     expect(double).toBe(4);
   });
 });
--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -1,17 +1,19 @@
 import reactive from '../reactive';
 
-describe("reactive", () => {
-  it("happy path", () => {
+describe('reactive', () => {
+  it('happy path', () => {
     const raw = {
-      count: 1
+      count: 1,
     };
 
+    // raw -> proxy
     const observed = reactive(raw);
 
     expect(observed.count).not.toBe(raw);
 
     expect(observed.count).toBe(1);
 
+    // update -> set
     observed.count++;
 
     expect(raw.count).toBe(2);
